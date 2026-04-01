675d5d396c2d45bd97b64febe9615e7d9e8c2b41
Alexander A. <mail.alexthegoood@gmail.com>
2026-04-01 17:00:47 +0300

    change PermissionManager logic

--- src/main/java/com/github/assemblyorg/keepInventory/permissions/PermissionManager.java
+++ src/main/java/com/github/assemblyorg/keepInventory/permissions/PermissionManager.java
@@ -1,26 +1,19 @@
 package com.github.assemblyorg.keepInventory.permissions;
 
-import org.bukkit.permissions.Permission;
 import org.bukkit.plugin.java.JavaPlugin;
-
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Map;
 
 public final class PermissionManager {
 
     private final JavaPlugin plugin;
-    private final List<Permission> permissions = new ArrayList<>();
 
     public PermissionManager(JavaPlugin plugin) {
         this.plugin = plugin;
-
-        permissions.add(new Permission(Permissions.COMMAND_SELF.id(), Permission.DEFAULT_PERMISSION));
-        permissions.add(new Permission(Permissions.COMMAND_OTHER.id(), Permission.DEFAULT_PERMISSION, Map.of(Permissions.COMMAND_SELF.name(), true)));
     }
 
     public void registerPermissions() {
-        for (Permission permission : permissions) plugin.getServer().getPluginManager().addPermission(permission);
+        for (Permissions permission : Permissions.values()) {
+            plugin.getServer().getPluginManager().addPermission(permission.permission());
+        }
     }
 
 }
--- src/main/java/com/github/assemblyorg/keepInventory/permissions/Permissions.java
+++ src/main/java/com/github/assemblyorg/keepInventory/permissions/Permissions.java
@@ -1,18 +1,47 @@
 package com.github.assemblyorg.keepInventory.permissions;
 
+import org.bukkit.permissions.Permission;
+import org.bukkit.permissions.PermissionDefault;
+import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
+
+import java.util.Map;
+
 public enum Permissions {
 
-    COMMAND_SELF("keepinventory.command.self"),
-    COMMAND_OTHER("keepinventory.command.other");
+    COMMAND_SELF("keepinventory.command.self", null, Permission.DEFAULT_PERMISSION, null),
+    COMMAND_OTHER("keepinventory.command.other", null, Permission.DEFAULT_PERMISSION, Map.of(COMMAND_SELF.id(), true));
 
     private final String id;
+    private final String description;
+    private final PermissionDefault permissionDefault;
+    private final Map<String, Boolean> children;
 
-    Permissions(String id) {
+    Permissions(@NotNull String id, @Nullable String description, @Nullable PermissionDefault permissionDefault, @Nullable Map<String, Boolean> children) {
         this.id = id;
+        this.description = description;
+        this.permissionDefault = permissionDefault;
+        this.children = children;
     }
 
     public String id() {
         return id;
+    }
+
+    public String description() {
+        return description;
+    }
+
+    public PermissionDefault permissionDefault() {
+        return permissionDefault;
+    }
+
+    public Map<String, Boolean> children() {
+        return children;
+    }
+
+    public Permission permission() {
+        return new Permission(id, description, permissionDefault, children);
     }
 
 }
