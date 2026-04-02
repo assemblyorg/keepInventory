91dc7ea3930b45a11232bd1fa4b67e728b61a64b
Alexander A. <mail.alexthegoood@gmail.com>
2026-04-02 15:56:04 +0300

    add @NotNull annotation

--- src/main/java/com/github/assemblyorg/keepInventory/permissions/PermissionManager.java
+++ src/main/java/com/github/assemblyorg/keepInventory/permissions/PermissionManager.java
@@ -1,12 +1,13 @@
 package com.github.assemblyorg.keepInventory.permissions;
 
 import org.bukkit.plugin.java.JavaPlugin;
+import org.jetbrains.annotations.NotNull;
 
 public final class PermissionManager {
 
     private final JavaPlugin plugin;
 
-    public PermissionManager(JavaPlugin plugin) {
+    public PermissionManager(@NotNull JavaPlugin plugin) {
         this.plugin = plugin;
     }
 
